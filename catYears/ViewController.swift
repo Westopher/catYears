721cfb2b13d4b7f1a1f766a69e1f09c38b3c2474
West Kraemer <WestKraemer@gmail.com>
2016-12-01 12:41:31 -0500

    connected ib elements in view controller

--- catYears/ViewController.swift
+++ catYears/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageTextfield: UITextField!
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    @IBAction func submitPressed(_ sender: Any) {
+        
+        print(ageTextfield.text)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
